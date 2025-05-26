c93c0dbaa7fdc1d86bb4677412324a14f8e698d0
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-26 11:47:31 +0700

    menambahkan konstruktor dan destruktor pada kelas dokter

--- asosiasi/asotiation.cpp
+++ asosiasi/asotiation.cpp
@@ -18,4 +18,14 @@ class dokter {
     public:
     string nama;
     vector<pasien*> daftar_pasien;
+    
+    dokter(string pNama) :nama(pNama){
+    cout << "Dokter \"" << nama << "\" ada\n";
 }
+~dokter(){
+    cout << "Dokter \"" << nama << "\" tidak ada\n";
+}
+
+void tambahPasien(pasien*);
+void cetakPasien();
+};
