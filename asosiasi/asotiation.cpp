cd333041f9ed98b129678371e1ad006bb32d296b
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-26 14:55:10 +0700

    Menambahkan class pasien dengan asosiasi ke dokter

--- asosiasi/asotiation.cpp
+++ asosiasi/asotiation.cpp
@@ -3,29 +3,34 @@
 using namespace std;
 
 class dokter;
-class pasien { //asosiasi
+class pasien
+{ // asosiasi
 public:
     string nama;
-    vector<dokter*> daftar_dokter;
-    pasien(string pNama) :nama(pNama){
-        cout << "Pasien" \"" << nama << "\" tidak ada\n"; 
-    }      
-    void tambahDokter(dokter*);
+    vector<dokter *> daftar_dokter;
+    pasien(string pNama) : nama(pNama)
+    {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter *);
     void cetakDokter();
 };
 
-class dokter {
-    public:
+class dokter
+{
+public:
     string nama;
-    vector<pasien*> daftar_pasien;
-    
-    dokter(string pNama) :nama(pNama){
-    cout << "Dokter \"" << nama << "\" ada\n";
-}
-~dokter(){
-    cout << "Dokter \"" << nama << "\" tidak ada\n";
-}
+    vector<pasien *> daftar_pasien;
 
-void tambahPasien(pasien*);
-void cetakPasien();
+    dokter(string pNama) : nama(pNama)
+    {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter()
+    {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien *);
+    void cetakPasien();
 };
