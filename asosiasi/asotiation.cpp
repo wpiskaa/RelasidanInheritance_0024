eee4f22aa8b9c1f74898f8acba44064f2af2d269
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-26 16:05:05 +0700

    Implementasi fungsi tambahDokter dan cetakDokter pada class pasien

--- asosiasi/asotiation.cpp
+++ asosiasi/asotiation.cpp
@@ -34,3 +34,14 @@ public:
     void tambahPasien(pasien *);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter){
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter(){
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
